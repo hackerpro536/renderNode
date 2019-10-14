1072807c466a7bf91404f724282c4bf34d11ddd6
Le Van Phu <vanphupc50@gmail.com>
2019-10-14 15:11:20 +0700

    add chrome

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-const render = require('prerender');
-const ssr = render();
-ssr.start();
+const prerender = require('prerender');
+var server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
+server.use(require('prerender-memory-cache'))
+server.start();
